86eeb6cb5ff37ccd9db6863431e5bbb6d8e0aefb
Alejandro <aroverayala@hotmail.com>
2025-03-25 22:42:40 -0300

    Primer Commit

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Ejercicio1 {
     public static void main(String[] args) {
         Scanner scr = new Scanner(System.in);
-        String nombre = "";
+        String nombre;
         System.out.println("Ingrese un nombre: ");
         nombre = scr.nextLine();
         System.out.println("Bienvenido al sistema " + nombre);
